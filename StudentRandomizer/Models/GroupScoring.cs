36cc3294317fcd57da78abf5185b8ad7efc18081
Min Chang <minchangmhc@gmail.com>
2021-06-14 16:42:53 -0700

    🚧 methods to return the score of each group.

--- StudentRandomizer/Models/GroupScoring.cs
+++ StudentRandomizer/Models/GroupScoring.cs
@@ -1,7 +1,45 @@
+using System.Linq;
+using System.Collections.Generic;
+
 namespace StudentRandomizer.Models
 {
   public class GroupScoring
   {
+    public string Name { get; set; }
+    public int StudentId { get; }
+    public int StudentFrequency { get; set; }
+
+    public int MatchFrequency(int StudentId, int MatchStudentId)
+    {
+      var db = Database.Open("StudentRandomizer");
+      int Frequency = db.Query("Match");
+      Stack<int> TempScore = new Stack<int>();
+      for (int i = 0; i < Match.Count; i++)
+      {
+        if (StudentId == MatchStudentId)
+        {
+          TempScore.Push(Frequency);
+        }
+      }
+      return TempScore;
+    }
+
+    public static int GroupScore(allGeneratedGroups)
+    {
+      for (int i = 0; i < allGeneratedGroups.Count; i++)
+      {
+        for (int j = 0; j < groupStudents.Count; j++)
+        {
+          MatchFrequency(j, groupStudents[j])
+        }
+      }
+
+    }
+    // loop through group list
+    //nested loop for each group
+    // in nested loop add for frequency for each pair
+    // group score == adding of each nested loop
+
     // public int Score { get; }
     // public GroupScoring(int score)
     // {
---
+++ StudentRandomizer/Models/Student.cs
@@ -0,0 +1,17 @@
+namespace StudentRandomizer.Models
+{
+  public class Student
+  {
+    public string Name { get; set; }
+    public Student(string name)
+    {
+      this.Name = name;
+    }
+    //40 
+    //s1 - 39 properties for matches
+    //find frequency of each pair
+    //each pair has a specific frequency by querying the database
+
+
+  }
+}
