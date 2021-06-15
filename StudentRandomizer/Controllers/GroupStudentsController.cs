a5326aeb0f87532901ecb6b75c71bf2bf417c2ac
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-15 15:36:19 -0700

    comments out join controllers
    Co-authored-by: Brent Hubbard <hubbardbrent@hotmail.com>

--- StudentRandomizer/Controllers/GroupStudentsController.cs
+++ StudentRandomizer/Controllers/GroupStudentsController.cs
@@ -1,113 +1,113 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using StudentRandomizer.Models;
-using System.Linq;
+// using System.Collections.Generic;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.EntityFrameworkCore;
+// using StudentRandomizer.Models;
+// using System.Linq;
 
-namespace StudentRandomizer.Controllers
-{
-  [ApiController]
-  [Route("api/[controller]")]
-  public class GroupStudentsController : ControllerBase
-  {
-    private readonly StudentRandomizerContext _db;
+// namespace StudentRandomizer.Controllers
+// {
+//   [ApiController]
+//   [Route("api/[controller]")]
+//   public class GroupStudentsController : ControllerBase
+//   {
+//     private readonly StudentRandomizerContext _db;
 
-    public GroupStudentsController(StudentRandomizerContext db)
-    {
-      _db = db;
-    }
+//     public GroupStudentsController(StudentRandomizerContext db)
+//     {
+//       _db = db;
+//     }
 
-    private bool GroupStudentExists(int id)
-    {
-      return _db.GroupStudent.Any(gs => gs.GroupStudentId == id);
-    }
+//     private bool GroupStudentExists(int id)
+//     {
+//       return _db.GroupStudent.Any(gs => gs.GroupStudentId == id);
+//     }
 
-    // POST
-    [HttpPost]
-    public async Task<ActionResult<GroupStudent>> CreateGroupStudent(GroupStudent groupStudent)
-    {
-      _db.GroupStudent.Add(groupStudent);
-      await _db.SaveChangesAsync();
-      return CreatedAtAction("Post", new { id = groupStudent.GroupStudentId },
-      groupStudent);
-    }
+//     // POST
+//     [HttpPost]
+//     public async Task<ActionResult<GroupStudent>> CreateGroupStudent(GroupStudent groupStudent)
+//     {
+//       _db.GroupStudent.Add(groupStudent);
+//       await _db.SaveChangesAsync();
+//       return CreatedAtAction("Post", new { id = groupStudent.GroupStudentId },
+//       groupStudent);
+//     }
 
-    // GET general
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<GroupStudent>>> Get(int studentId)
-    {
-      var query = _db.GroupStudent.AsQueryable();
+//     // GET general
+//     [HttpGet]
+//     public async Task<ActionResult<IEnumerable<GroupStudent>>> Get(int studentId)
+//     {
+//       var query = _db.GroupStudent.AsQueryable();
 
-      if (studentId != null)
-      {
-        query = query.Where(gs => gs.StudentId == studentId);
-      }
+//       if (studentId != null)
+//       {
+//         query = query.Where(gs => gs.StudentId == studentId);
+//       }
 
-      return await query.ToListAsync();
-    }
+//       return await query.ToListAsync();
+//     }
 
-    // GET with id
-    [HttpGet("{id}")]
-    public async Task<ActionResult<GroupStudent>> GetGroupStudent(int id)
-    {
-      var groupStudent = await _db.GroupStudent.FirstOrDefaultAsync(gs => gs.GroupStudentId == id);
+//     // GET with id
+//     [HttpGet("{id}")]
+//     public async Task<ActionResult<GroupStudent>> GetGroupStudent(int id)
+//     {
+//       var groupStudent = await _db.GroupStudent.FirstOrDefaultAsync(gs => gs.GroupStudentId == id);
 
-      if (groupStudent == null)
-      {
-        return NotFound();
-      }
+//       if (groupStudent == null)
+//       {
+//         return NotFound();
+//       }
 
-      return groupStudent;
-    }
+//       return groupStudent;
+//     }
 
-    // PUT with id
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Put(int id, GroupStudent groupStudent)
-    {
-      if (id != groupStudent.GroupStudentId)
-      {
-        return BadRequest();
-      }
+//     // PUT with id
+//     [HttpPut("{id}")]
+//     public async Task<IActionResult> Put(int id, GroupStudent groupStudent)
+//     {
+//       if (id != groupStudent.GroupStudentId)
+//       {
+//         return BadRequest();
+//       }
 
-      _db.Entry(groupStudent).State = EntityState.Modified;
+//       _db.Entry(groupStudent).State = EntityState.Modified;
 
-      try
-      {
-        await _db.SaveChangesAsync();
-      }
-      catch (DbUpdateConcurrencyException)
-      {
-        if (!GroupStudentExists(id))
-        {
-          return NotFound();
-        }
-        else
-        {
-          throw;
-        }
+//       try
+//       {
+//         await _db.SaveChangesAsync();
+//       }
+//       catch (DbUpdateConcurrencyException)
+//       {
+//         if (!GroupStudentExists(id))
+//         {
+//           return NotFound();
+//         }
+//         else
+//         {
+//           throw;
+//         }
 
-      }
+//       }
       
-      return NoContent();
-    }
+//       return NoContent();
+//     }
 
-    // DELETE with id
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteGroupStudent(int id)
-    {
-      var groupStudent = await _db.GroupStudent.FindAsync(id);
+//     // DELETE with id
+//     [HttpDelete("{id}")]
+//     public async Task<IActionResult> DeleteGroupStudent(int id)
+//     {
+//       var groupStudent = await _db.GroupStudent.FindAsync(id);
 
-      if (groupStudent == null)
-      {
-        return NotFound();
-      }
+//       if (groupStudent == null)
+//       {
+//         return NotFound();
+//       }
 
-      _db.GroupStudent.Remove(groupStudent);
-      await _db.SaveChangesAsync();
+//       _db.GroupStudent.Remove(groupStudent);
+//       await _db.SaveChangesAsync();
 
-      return NoContent();
-    }
+//       return NoContent();
+//     }
 
-  }
-}
+//   }
+// }
--- StudentRandomizer/Controllers/MatchStudentsController.cs
+++ StudentRandomizer/Controllers/MatchStudentsController.cs
@@ -1,113 +1,113 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using StudentRandomizer.Models;
-using System.Linq;
+// using System.Collections.Generic;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.EntityFrameworkCore;
+// using StudentRandomizer.Models;
+// using System.Linq;
 
-namespace StudentRandomizer.Controllers
-{
-  [ApiController]
-  [Route("api/[controller]")]
-  public class MatchStudentsController : ControllerBase
-  {
-    private readonly StudentRandomizerContext _db;
+// namespace StudentRandomizer.Controllers
+// {
+//   [ApiController]
+//   [Route("api/[controller]")]
+//   public class MatchStudentsController : ControllerBase
+//   {
+//     private readonly StudentRandomizerContext _db;
 
-    public MatchStudentsController(StudentRandomizerContext db)
-    {
-      _db = db;
-    }
+//     public MatchStudentsController(StudentRandomizerContext db)
+//     {
+//       _db = db;
+//     }
 
-    private bool MatchStudentExists(int id)
-    {
-      return  _db.MatchStudent.Any(ms => ms.MatchStudentId == id);
-    }
+//     private bool MatchStudentExists(int id)
+//     {
+//       return  _db.MatchStudent.Any(ms => ms.MatchStudentId == id);
+//     }
 
-    // POST
-    [HttpPost]
-    public async Task<ActionResult<MatchStudent>> CreateMatchStudent(MatchStudent matchStudent)
-    {
-      _db.MatchStudent.Add(matchStudent);
-      await _db.SaveChangesAsync();
-      return CreatedAtAction("Post", new { id = matchStudent.MatchStudentId }, matchStudent);
-    }
+//     // POST
+//     [HttpPost]
+//     public async Task<ActionResult<MatchStudent>> CreateMatchStudent(MatchStudent matchStudent)
+//     {
+//       _db.MatchStudent.Add(matchStudent);
+//       await _db.SaveChangesAsync();
+//       return CreatedAtAction("Post", new { id = matchStudent.MatchStudentId }, matchStudent);
+//     }
 
-    // GET general
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<MatchStudent>>> Get(int studentId)
-    {
-      var query = _db.MatchStudent.AsQueryable();
+//     // GET general
+//     [HttpGet]
+//     public async Task<ActionResult<IEnumerable<MatchStudent>>> Get(int studentId)
+//     {
+//       var query = _db.MatchStudent.AsQueryable();
 
-      if (studentId != null)
-      {
-        query = query.Where(ms => ms.StudentId == studentId);
-      }
+//       if (studentId != null)
+//       {
+//         query = query.Where(ms => ms.StudentId == studentId);
+//       }
 
-      return await query.ToListAsync();
-    }
+//       return await query.ToListAsync();
+//     }
 
-    // GET with id
-    [HttpGet("{id}")]
-    public async Task<ActionResult<MatchStudent>> GetMatchStudent(int id)
-    {
-      var matchStudent = await _db.MatchStudent.FirstOrDefaultAsync(ms => ms.MatchStudentId == id);
+//     // GET with id
+//     [HttpGet("{id}")]
+//     public async Task<ActionResult<MatchStudent>> GetMatchStudent(int id)
+//     {
+//       var matchStudent = await _db.MatchStudent.FirstOrDefaultAsync(ms => ms.MatchStudentId == id);
 
-      if (matchStudent == null)
-      {
-        return NotFound();
-      }
+//       if (matchStudent == null)
+//       {
+//         return NotFound();
+//       }
 
-      return matchStudent;
-    }
+//       return matchStudent;
+//     }
 
-    // PUT
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Put(int id, MatchStudent matchStudent)
-    {
-      if (id != matchStudent.MatchStudentId)
-      {
-        return BadRequest();
-      }
+//     // PUT
+//     [HttpPut("{id}")]
+//     public async Task<IActionResult> Put(int id, MatchStudent matchStudent)
+//     {
+//       if (id != matchStudent.MatchStudentId)
+//       {
+//         return BadRequest();
+//       }
 
-      _db.Entry(matchStudent).State = EntityState.Modified;
+//       _db.Entry(matchStudent).State = EntityState.Modified;
 
-      try
-      {
-        await _db.SaveChangesAsync();
-      }
-      catch (DbUpdateConcurrencyException)
-      {
-        if (!MatchStudentExists(id))
-        {
-          return NotFound();
-        }
-        else
-        {
-          throw;
-        }
+//       try
+//       {
+//         await _db.SaveChangesAsync();
+//       }
+//       catch (DbUpdateConcurrencyException)
+//       {
+//         if (!MatchStudentExists(id))
+//         {
+//           return NotFound();
+//         }
+//         else
+//         {
+//           throw;
+//         }
 
-      }
+//       }
       
-      return NoContent();
-    }
+//       return NoContent();
+//     }
 
-    // DELETE
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteMatchStudent(int id)
-    {
-      var matchStudent = await _db.MatchStudent.FindAsync(id);
+//     // DELETE
+//     [HttpDelete("{id}")]
+//     public async Task<IActionResult> DeleteMatchStudent(int id)
+//     {
+//       var matchStudent = await _db.MatchStudent.FindAsync(id);
 
-      if (matchStudent == null)
-      {
-        return NotFound();
-      }
+//       if (matchStudent == null)
+//       {
+//         return NotFound();
+//       }
 
-      _db.MatchStudent.Remove(matchStudent);
-      await _db.SaveChangesAsync();
+//       _db.MatchStudent.Remove(matchStudent);
+//       await _db.SaveChangesAsync();
 
-      return NoContent();
-    }
+//       return NoContent();
+//     }
 
 
-  }
-}
+//   }
+// }
