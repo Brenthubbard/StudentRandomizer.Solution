78c648c3a3e0f38d423e3d92077bfa1f8edd5983
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-15 11:50:12 -0700

    comments out redirection and authorization
    Co-authored-by: Brent Hubbard <hubbardbrent@hotmail.com>

--- StudentRandomizer/Startup.cs
+++ StudentRandomizer/Startup.cs
@@ -1,14 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-// using Microsoft.AspNetCore.HttpsPolicy;
-// using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-// using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using StudentRandomizer.Models;
+// using Microsoft.Extensions.Logging;
+// using Microsoft.AspNetCore.HttpsPolicy;
+// using Microsoft.AspNetCore.Mvc;
 // using System;
 // using System.Collections.Generic;
 // using System.Linq;
@@ -49,11 +49,11 @@ namespace StudentRandomizer
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "StudentRandomizer v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            // app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
