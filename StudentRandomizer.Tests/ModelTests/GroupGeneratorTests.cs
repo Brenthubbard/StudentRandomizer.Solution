91080f1b41f1dc130880d485e267b83b036858e1
Vanessa Su <vnessa.su@gmail.com>
2021-06-15 10:22:24 -0400

    :white_check_mark: Add test files for GroupGenerator

---
+++ StudentRandomizer.Tests/ModelTests/GroupGeneratorTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentRandomizer.Models;
+using System;
+using System.Collections.Generic;
+
+namespace StudentRandomizer.Tests
+{
+  [TestClass]
+  public class GroupGeneratorTests
+  {
+    [TestMethod]
+    public void GenerateAllPossibleGroups_StudentList_GroupList()
+    {
+
+    }
+  }
+}
