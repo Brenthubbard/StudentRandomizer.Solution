c1a9b3aee311fdb94fc49a84162ac04e6f64b0c3
Vanessa Su <vnessa.su@gmail.com>
2021-06-15 10:23:15 -0400

    :poop: Add bad test for functionality check

--- StudentRandomizer.Tests/ModelTests/GroupGeneratorTests.cs
+++ StudentRandomizer.Tests/ModelTests/GroupGeneratorTests.cs
@@ -11,7 +11,25 @@ namespace StudentRandomizer.Tests
     [TestMethod]
     public void GenerateAllPossibleGroups_StudentList_GroupList()
     {
-
+      int numberOfStudents = 5;
+      int groupSize = 3;
+      List<Student> listOfStudents = new List<Student>();
+      for (int i = 1; i <= numberOfStudents; i++)
+      {
+        Student newStudent = new Student("Student" + i.ToString());
+        listOfStudents.Add(newStudent);
+      }
+      List<Group> allGroups = GroupGenerator.GenerateAllPossibleGroups(listOfStudents, groupSize);
+      int groupNum = 0;
+      foreach (Group group in allGroups)
+      {
+        groupNum++;
+        Console.WriteLine("\nGroup {0}", groupNum);
+        foreach (Student student in group.DevTeamStudents)
+        {
+          Console.WriteLine(student.Name);
+        }
+      }
     }
   }
 }
