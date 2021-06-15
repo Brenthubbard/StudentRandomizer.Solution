cdefd58d0ce9705a3924ed2f3d9812f78b7e5491
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-14 17:09:54 -0700

    uncomments match score and frequency
    Co-authored-by: Brent Hubbard <hubbardbrent@hotmail.com>

--- StudentRandomizer/Models/Match.cs
+++ StudentRandomizer/Models/Match.cs
@@ -6,10 +6,10 @@ namespace StudentRandomizer.Models
   public class Match
   {
     public int MatchId { get; set;}
-    public int MatchScore { get; set; } // should this be in the join entity?
+    public int MatchScore { get; set; }
 
     public List<Student> Students { get; set; }
-    // public int MatchFreq { get; set; }
+    public int MatchFreq { get; set; }
     public virtual ICollection<MatchStudent> MatchStudentJoinEntities { get; set; }
     public Match()   
     {
