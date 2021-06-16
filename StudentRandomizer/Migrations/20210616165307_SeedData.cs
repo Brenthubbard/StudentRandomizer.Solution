ebf1844970664c4c081da4f72d2c4a247a1e8557
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-16 09:53:42 -0700

    adds seed data and corresponding migrations

---
+++ StudentRandomizer/Migrations/20210616165307_SeedData.cs
@@ -0,0 +1,256 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentRandomizer.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Groups",
+                column: "GroupId",
+                values: new object[]
+                {
+                    1,
+                    2,
+                    3,
+                    4,
+                    5
+                });
+
+            migrationBuilder.InsertData(
+                table: "Matches",
+                columns: new[] { "MatchId", "MatchScore" },
+                values: new object[,]
+                {
+                    { 1, 0 },
+                    { 2, 0 },
+                    { 3, 0 },
+                    { 4, 0 },
+                    { 5, 0 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentId", "Name" },
+                values: new object[,]
+                {
+                    { 16, "Michael Burton" },
+                    { 17, "Min Chang" },
+                    { 18, "Nicholas Reeder" },
+                    { 19, "Patrick Lee" },
+                    { 20, "Ryan Walker" },
+                    { 25, "Tiffany Greathead" },
+                    { 22, "Seth Medeiros" },
+                    { 23, "Thomas Friedrichs" },
+                    { 24, "Thomas Russell" },
+                    { 15, "Marney Mallory" },
+                    { 21, "Sammai Gutierrez" },
+                    { 14, "Jonathan Stull" },
+                    { 9, "James Wyn" },
+                    { 12, "Jesse White" },
+                    { 11, "Jeremy Banka" },
+                    { 10, "Jamie Knutsen" },
+                    { 26, "Tom Geraghty" },
+                    { 8, "Isaac Moreno" },
+                    { 7, "Giancarlo Vigneri" },
+                    { 6, "Cristina Plesa" },
+                    { 5, "Cassandra Copp" },
+                    { 4, "Carlos Mendez" },
+                    { 3, "Brent Hubbard" },
+                    { 2, "Brandon Magofna" },
+                    { 1, "Ahmed Ghouzlane" },
+                    { 13, "John Edmondson" },
+                    { 27, "Vanessa Su" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "GroupId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "GroupId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "GroupId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "GroupId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "GroupId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Matches",
+                keyColumn: "MatchId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Matches",
+                keyColumn: "MatchId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Matches",
+                keyColumn: "MatchId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Matches",
+                keyColumn: "MatchId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Matches",
+                keyColumn: "MatchId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 23);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 24);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 25);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 26);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 27);
+        }
+    }
+}
--- StudentRandomizer/Models/StudentRandomizerContext.cs
+++ StudentRandomizer/Models/StudentRandomizerContext.cs
@@ -16,6 +16,56 @@ namespace StudentRandomizer.Models
 
     public StudentRandomizerContext(DbContextOptions<StudentRandomizerContext> options) : base(options) { }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Student>()
+            .HasData(
+              new Student { StudentId = 1, Name = "Ahmed Ghouzlane" },
+              new Student { StudentId = 2, Name = "Brandon Magofna" },
+              new Student { StudentId = 3, Name = "Brent Hubbard" },
+              new Student { StudentId = 4, Name = "Carlos Mendez" },
+              new Student { StudentId = 5, Name = "Cassandra Copp" },
+              new Student { StudentId = 6, Name = "Cristina Plesa" },
+              new Student { StudentId = 7, Name = "Giancarlo Vigneri" },
+              new Student { StudentId = 8, Name = "Isaac Moreno" },
+              new Student { StudentId = 9, Name = "James Wyn" },
+              new Student { StudentId = 10, Name = "Jamie Knutsen" },
+              new Student { StudentId = 11, Name = "Jeremy Banka" },
+              new Student { StudentId = 12, Name = "Jesse White" },
+              new Student { StudentId = 13, Name = "John Edmondson" },
+              new Student { StudentId = 14, Name = "Jonathan Stull" },
+              new Student { StudentId = 15, Name = "Marney Mallory" },
+              new Student { StudentId = 16, Name = "Michael Burton" },
+              new Student { StudentId = 17, Name = "Min Chang" },
+              new Student { StudentId = 18, Name = "Nicholas Reeder" },
+              new Student { StudentId = 19, Name = "Patrick Lee" },
+              new Student { StudentId = 20, Name = "Ryan Walker" },
+              new Student { StudentId = 21, Name = "Sammai Gutierrez" },
+              new Student { StudentId = 22, Name = "Seth Medeiros" },
+              new Student { StudentId = 23, Name = "Thomas Friedrichs" },
+              new Student { StudentId = 24, Name = "Thomas Russell" },
+              new Student { StudentId = 25, Name = "Tiffany Greathead" },
+              new Student { StudentId = 26, Name = "Tom Geraghty" },
+              new Student { StudentId = 27, Name = "Vanessa Su" }
+            );
+      builder.Entity<Match>()
+            .HasData(
+              new Match { MatchId = 1 },
+              new Match { MatchId = 2 },
+              new Match { MatchId = 3 },
+              new Match { MatchId = 4 },
+              new Match { MatchId = 5 }
+            );
+      builder.Entity<Group>()
+            .HasData(
+              new Group { GroupId = 1 },
+              new Group { GroupId = 2 },
+              new Group { GroupId = 3 },
+              new Group { GroupId = 4 },
+              new Group { GroupId = 5 }
+            );
+    }
+
     // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     // {
     //   optionsBuilder.UseLazyLoadingProxies();
